441b239ad34d7504adb0f8b824bef5462acd3d77
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2018-12-10 21:26:09 +0100

    Fix live mode

--- pygears_view/main.py
+++ pygears_view/main.py
@@ -10,16 +10,17 @@ from pygears_view.graph import graph
 from pygears_view.which_key import which_key
 from pygears_view.gtkwave import gtkwave
 from pygears_view.sniper import sniper
-from pygears.conf import Inject, reg_inject, safe_bind, PluginBase
+from pygears.conf import Inject, reg_inject, safe_bind, PluginBase, registry
 from .pygears_proxy import PyGearsBridgeServer, PyGearsManager, sim_bridge
 
 
 class PyGearsView(PyGearsBridgeServer):
-    @reg_inject
     def __init__(self, top=None, live=False):
         super().__init__(top)
         self.live = live
         self.pipe = None
+        if live:
+            registry('viewer/layers').insert(0, sim_bridge)
 
     @reg_inject
     def before_run(self, sim, outdir=Inject('sim/artifact_dir')):
@@ -30,8 +31,8 @@ class PyGearsView(PyGearsBridgeServer):
             thread.start()
 
             self.pipe, qt_pipe = multiprocessing.Pipe()
-            safe_bind('viewer/sim_bridge_pipe', qt_pipe)
-            p = multiprocessing.Process(target=main)
+
+            p = multiprocessing.Process(target=main, args=(qt_pipe, ))
             p.start()
 
     def after_cleanup(self, sim):
@@ -40,7 +41,10 @@ class PyGearsView(PyGearsBridgeServer):
 
 
 @reg_inject
-def main(layers=Inject('viewer/layers')):
+def main(pipe=None, layers=Inject('viewer/layers')):
+    if pipe:
+        safe_bind('viewer/sim_bridge_pipe', pipe)
+
     app = QtWidgets.QApplication(sys.argv)
     app.setFont(QtGui.QFont("DejaVu Sans Mono", 13))
 
@@ -56,7 +60,4 @@ def main(layers=Inject('viewer/layers')):
 class SimPlugin(PluginBase):
     @classmethod
     def bind(cls):
-        safe_bind(
-            'viewer/layers',
-            # [sim_bridge, graph, which_key, gtkwave, sniper])
-            [which_key, graph, gtkwave, sniper])
+        safe_bind('viewer/layers', [which_key, graph, gtkwave, sniper])
--- pygears_view/pygears_proxy.py
+++ pygears_view/pygears_proxy.py
@@ -1,9 +1,6 @@
-import threading
-import multiprocessing
-
-from PySide2 import QtCore
+from PySide2 import QtCore, QtWidgets
 from pygears import registry
-from pygears.conf import Inject, reg_inject, safe_bind
+from pygears.conf import MayInject, reg_inject, safe_bind
 from multiprocessing.managers import BaseManager
 from pygears.sim.extens.sim_extend import SimExtend
 
@@ -27,17 +24,18 @@ class PyGearsBridgeServer(SimExtend):
     def after_timestep(self, sim, timestep):
         if self.pipe:
             self.pipe.send("after_timestep")
+            # return True
 
     def after_run(self, sim):
         if self.pipe:
             self.pipe.send("after_run")
 
 
-def sim_bridge(pipe=Inject('viewer/sim_bridge_pipe')):
+def sim_bridge():
     manager = PyGearsManager(address=('', 5000))
     manager.connect()
 
-    sim_bridge = PyGearsClient(pipe)
+    sim_bridge = PyGearsClient()
     safe_bind('viewer/sim_bridge', sim_bridge)
     safe_bind('viewer/sim_proxy', manager)
 
@@ -48,31 +46,50 @@ class PyGearsClient(QtCore.QObject):
     sim_refresh = QtCore.Signal()
     message = QtCore.Signal(str)
 
-    def __init__(self, pipe, step=False, resfresh_interval=1000, parent=None):
+    @reg_inject
+    def __init__(self,
+                 pipe=MayInject('viewer/sim_bridge_pipe'),
+                 step=False,
+                 refresh_interval=1000,
+                 parent=None):
         super().__init__(parent)
 
-        if resfresh_interval:
-            self.after_timestep_timer = QtCore.QTimer()
-            self.after_timestep_timer.timeout.connect(self.refresh_timeout)
-            self.after_timestep_timer.start(resfresh_interval)
-
+        self.pipe = pipe
         self.step = step
+        self.refresh_interval = refresh_interval
+
+        QtWidgets.QApplication.instance().aboutToQuit.connect(self.quit)
         self.thrd = QtCore.QThread()
         self.moveToThread(self.thrd)
         self.thrd.started.connect(self.run)
         self.thrd.start()
 
     def run(self):
-        nop = QtCore.QTimer(self)
+        if self.refresh_interval:
+            self.after_timestep_timer = QtCore.QTimer(self)
+            self.after_timestep_timer.timeout.connect(self.refresh_timeout)
+            self.after_timestep_timer.start(self.refresh_interval)
+
         while True:
-            nop.start()
+            self.thrd.eventDispatcher().processEvents(
+                QtCore.QEventLoop.AllEvents)
+
+            if not self.pipe.poll(0.01):
+                continue
+
             msg = self.pipe.recv()
+
             self.message.emit(msg)
             if msg == "after_run":
                 self.after_timestep_timer.stop()
                 self.after_run.emit()
+                self.quit()
+                return
             elif msg == "after_timestep" and self.step:
                 self.after_timestep.emit()
+
+    def quit(self):
+        self.thrd.terminate()
 
     def refresh_timeout(self):
         self.sim_refresh.emit()
